ed7e145ba077dd5ca3a2d9016ea5d2524caade33
Tanay <chandak.tanay@gmail.com>
2022-05-02 00:10:56 -0400

    Update Results.js

--- Frontend/nba-front/src/components/Results.js
+++ Frontend/nba-front/src/components/Results.js
@@ -1,15 +1,41 @@
 import { useEffect, useState } from "react";
+import Sidebar from "./Sidebar.js";
+import Query from "./Query.js";
 
-const Results = () => {
+const Results = (props) => {
     const query = window.location.href.split('query/')[1];
-    const [results, setResults] = useState([]);
+    const [results, setResults] = useState([{"unkown": ""}]);
     
     useEffect(() => {
         fetch("http://localhost:3000/" + query)
             .then(results => results.json())
             .then(data => {setResults(data.results)})
     }, [query])
-    return <div></div>
+
+    return (<div>
+        <div className="flex flex-row h-screen overflow-y-scroll">
+            <div className="basis-1/4 h-full">
+                <Sidebar currentState={props.currentState}>
+                </Sidebar>
+            </div>
+            <div className="basis-3/4 h-screen overflow-y-scroll">
+                <table className="overflow-x-scroll m-4">
+                    <thead>
+                        <tr>{Object.keys(results[0]).map(key => <th>{key}</th>)}</tr>
+                    </thead>
+                    <tbody>
+                        {results.map(result => 
+                        <tr>
+                            {Object.keys(result).map(key => 
+                            <td>
+                                {result[key]}
+                            </td>)}
+                        </tr>)}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>)
 }
 
 export default Results
